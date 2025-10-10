5deaefd8a3fd0fbf94752ef295e88bac52916974
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2025-10-10 12:00:51 +0300

    added the rest of the services and added the docker files in each service

---
+++ backend/disccovery/src/test/java/com/example/disccovery/DisccoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disccovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisccoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
