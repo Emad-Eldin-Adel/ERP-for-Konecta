d08298042cd83a34b7f892fb1291fd2e8faa87e6
Abdallah <abdallah2110678@miuegypt.edu.eg>
2025-11-21 21:08:22 +0200

    redesigning the frontend

--- src/backend/finance-service/obj/Debug/net9.0/finance-service.AssemblyInfo.cs
+++ src/backend/finance-service/obj/Debug/net9.0/finance-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d624f491eee9c82b99372cc8534cbd258799a7bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919dc733ca939866613d79bd9702056d7b73a0ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
