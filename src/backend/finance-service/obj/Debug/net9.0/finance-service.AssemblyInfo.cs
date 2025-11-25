f1f88662efe2f08d941fec22894479dddb126a10
Abdallah <abdallah2110678@miuegypt.edu.eg>
2025-11-25 20:50:58 +0200

    updated the readme with the deployment link

--- src/backend/finance-service/obj/Debug/net9.0/finance-service.AssemblyInfo.cs
+++ src/backend/finance-service/obj/Debug/net9.0/finance-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914e01f84fefd3fcc51ec875c0ccf0897012aef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec7e24b688b611617cb9ef81df07e2a186cab07")]
 [assembly: System.Reflection.AssemblyProductAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finance-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
